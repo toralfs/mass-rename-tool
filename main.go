c2edeb3acfebfec4e9b1b031683f88fa66901afd
toralfs <toralfs97@gmail.com>
2020-08-06 23:53:29 +0200

    program actually works now. Needs a lot of refinement but that's okay

--- main.go
+++ main.go
@@ -9,11 +9,26 @@ import (
 )
 
 func main() {
+	fmt.Println(`
+--------------------------------------------
+Welcome to Mass Rename Tool
+		
+Program will look from root directory after 
+a specific filename and rename to chosen name
+--------------------------------------------
+`)
+	fmt.Println("Enter file name to look for: ")
+
+	var oldName, newName string
+	fmt.Scan(&oldName)
+	fmt.Println("Enter new file name: ")
+	fmt.Scan(&newName)
+
 	files, err := walkDir("files/")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	renameFiles(files, "README.md")
+	renameFiles(files, string(oldName), string(newName))
 }
 
 func walkDir(root string) ([]string, error) {
@@ -27,15 +42,13 @@ func walkDir(root string) ([]string, error) {
 	return files, err
 }
 
-func renameFiles(files []string, newName string) {
-	for _, v := range files {
-		fmt.Println(v)
-		xs := strings.SplitAfter(v, `\`)
-		if len(xs) > 1 {
-			//newPath := fmt.Sprintf("%s%s", strings.Trim(fmt.Sprint(xs[:len(xs)-1]), "[]"), newName)
-			newPath := strings.Trim(strings.Join(xs, " "), " ")
-			fmt.Println(newPath)
-
+func renameFiles(files []string, oldName string, newName string) {
+	for _, oldPath := range files {
+		newPath := strings.ReplaceAll(oldPath, oldName, newName)
+		err := os.Rename(oldPath, newPath)
+		if err != nil {
+			log.Printf("Could not rename file at path: %s, error: %v", oldPath, err)
+			continue
 		}
 	}
 }
